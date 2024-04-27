1d322bc73ddde5e7bb8e43aba40d665d57b20c42
HarkHorning <hjhorning@gmai.com>
2024-04-27 14:04:01 -0400

    Got lighting to work properly

--- Three-D-Web-Site/main.js
+++ Three-D-Web-Site/main.js
@@ -1,47 +1,71 @@
 import './style.css'
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
+window.addEventListener('load', () => {
 
-// objects
-// const geometry = new THREE.SphereGeometry(3, 64, 64);
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial({
-  color: '#FF6347',
-  // wireframe: true,
-  roughness: 1,
-  metalness: 0.25
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+  const scene = new THREE.Scene();
 
-// light
-const light = new THREE.PointLight( 0xff0000, 1, 100 );
-light.position.set( 50, 50, 50 );
-scene.add( light );
+  // objects
+  const sphere = new THREE.SphereGeometry(3, 16, 16);
+  const sphereTwo = new THREE.SphereGeometry(2, 16, 16);
+  const torus = new THREE.TorusGeometry( 10, 1, 16, 100)
+  const material = new THREE.MeshStandardMaterial({
+    color: '#FF6347',
+    // wireframe: true,
+    roughness: 0.75,
+    metalness: 0.5
 
-const ambLight = new THREE.AmbientLight();
-// scene.add( ambLight );
+  });
+  const materialTwo = new THREE.MeshStandardMaterial({
+    color: '#ff0000',
+    wireframe: true,
+    roughness: 1,
+    metalness: 0
+  });
+  const mesh = new THREE.Mesh(torus, material);
+  const meshOrbit = new THREE.Mesh(sphereTwo, material);
+  const meshSphere = new THREE.Mesh(sphere, material);
+  meshOrbit.position.set(0, 20, 0)
+  scene.add(mesh, meshSphere, meshOrbit);
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(25, 25, 25);
-scene.add(pointLight, ambLight, light);
+  // light
+  const light = new THREE.PointLight('#FFFFFF', 100, 100)
+  light.position.set( 50, 50, 50 );
+  scene.add( light );
 
-//camera
-const camera = new THREE.PerspectiveCamera( 75, 800 / 600, 0.1, 1000 );
-camera.position.z = 40;
-scene.add(camera);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, Math.PI)
+  directionalLight.position.set(1, 0, 1)
+  scene.add(directionalLight)
 
-//renderer
-const canvas = document.getElementById('webGL');
-const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
+  const ambLight = new THREE.AmbientLight();
+  // scene.add( ambLight );
+
+  //camera
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 
-function cycle(){
-  requestAnimationFrame(cycle);
-  renderer.render(scene, camera)
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
-  mesh.rotation.z += 0.01
-}; cycle();
+  let cameraZoom = 20;
+  camera.position.z = cameraZoom;
+  window.addEventListener('scroll', () => {
+    const scrollAmount = window.scrollY / 4;
+    let cameraZ = cameraZoom + scrollAmount;
+    camera.position.z = cameraZ;
+  })
+
+
+  scene.add(camera);
+
+  //renderer
+  const canvas = document.getElementById('webGL');
+  const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.setClearColor( 0xffffff );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  function cycle(){
+    requestAnimationFrame(cycle);
+    renderer.render(scene, camera)
+    mesh.rotation.x += 0.005
+    mesh.rotation.y += 0.005
+    mesh.rotation.z += 5
+  }; cycle();
+})
